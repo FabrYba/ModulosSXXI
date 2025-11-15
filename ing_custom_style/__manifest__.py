ae0321022643c0db1ee9f29fc80e0bb02ff364dc
FabrYba <fabrizioybaceta@gmail.com>
2025-11-15 20:50:51 -0300

    [NEW] ing_custom_style

---
+++ ing_custom_style/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Custom input styles',
+    'summary': """  """,
+    'author': 'Ybaceta Fabrizio',
+    'website': 'https://www.linkedin.com/in/fybaceta/',
+    'category': 'Style',
+    'version': '1.0',
+        'depends': ['web'],
+    'assets': {
+        'web.assets_backend': [
+            'ing_custom_style/static/src/css/custom_input_styles.css',
+        ],
+    },
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
